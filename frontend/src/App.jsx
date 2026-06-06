86acd10b68d5beabf89f7796587bb9a54106226a
jcastroj1-cj <jcastroj1@unicartagena.edu.co>
2026-06-06 16:32:16 -0500

    se modifica el vite.js y el users.js para que nos permita crear usuario, editar, eliminar, Hcer CRUD

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,15 @@
 import { Routes, Route, Navigate } from 'react-router-dom'
-import Login from './pages/Login'
 import Users from './pages/Users'
 import CreateUser from './pages/CreateUser'
 import EditUser from './pages/EditUser'
 
 function App() {
-  const isAuthenticated = () => !!localStorage.getItem('user')
-
   return (
     <Routes>
-      <Route path="/" element={<Navigate to="/login" />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/users" element={isAuthenticated() ? <Users /> : <Navigate to="/login" />} />
-      <Route path="/users/create" element={isAuthenticated() ? <CreateUser /> : <Navigate to="/login" />} />
-      <Route path="/users/edit/:id" element={isAuthenticated() ? <EditUser /> : <Navigate to="/login" />} />
+      <Route path="/" element={<Navigate to="/users" />} />
+      <Route path="/users" element={<Users />} />
+      <Route path="/users/create" element={<CreateUser />} />
+      <Route path="/users/edit/:id" element={<EditUser />} />
     </Routes>
   )
 }
--- frontend/src/api/users.js
+++ frontend/src/api/users.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const API = axios.create({
-  baseURL: 'http://localhost:8080/api',
+  baseURL: '/api',
 })
 
 export const getUsers = () => API.get('/users')
